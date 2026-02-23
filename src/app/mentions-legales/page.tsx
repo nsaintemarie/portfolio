fb3833ad45a75ebc6b0103cd05cabe5ae62809da
nsaintemarie <107178621+nsaintemarie@users.noreply.github.com>
2026-02-23 13:59:42 +0100

    clean : test deployment

--- src/app/mentions-legales/page.tsx
+++ src/app/mentions-legales/page.tsx
@@ -30,7 +30,6 @@ export const metadata: Metadata = {
 
 
 export default function MentionsLegales() {
-  console.log('test')
   return (
     <section className="relative py-12 md:py-20">
       <GridBackground />
